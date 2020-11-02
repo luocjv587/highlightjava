4080bb6721f83e2db28c2e755852354245c2133a
Luo <547605677@qq.com>
2020-11-02 14:30:07 +0800

    3.5 组合注解

---
+++ src/main/java/com/luocj/demo/ch3/annotation/DemoConfig.java
@@ -0,0 +1,5 @@
+package com.luocj.demo.ch3.annotation;
+
+@WiselyConfiguration("com.luocj.demo.ch3.annotation")
+public class DemoConfig {
+}
---
+++ src/main/java/com/luocj/demo/ch3/annotation/DemoService.java
@@ -0,0 +1,10 @@
+package com.luocj.demo.ch3.annotation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public void outputResult(){
+        System.out.println("从组合注解配置照样获得的bean");
+    }
+}
